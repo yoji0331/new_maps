ec143e8e896475f99541fce1d4d7efbdd0fb2f36
yoji0331 <smhsc251@yahoo.co.jp>
2017-01-11 03:37:29 +0900

    sendgridの有効化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'wesi.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
