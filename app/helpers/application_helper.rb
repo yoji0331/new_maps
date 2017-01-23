3241ab399133a8a7bc3c3c8cb842bd203fe82d76
yoji0331 <smhsc251@yahoo.co.jp>
2017-01-23 18:24:26 +0900

    時間の表示のヘルパーの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def full_title(page_title = "")
 		base_title = "水しるべ調査"
 	end
+	def show_dat(time)
+    return time unless time
+    time.strftime("%Y/%m/%d")
+  end
+
+  def show_at(time)
+    return time unless time
+    time.strftime("%Y/%m/%d %H:%M:%S")
+  end
 end
